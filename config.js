bab7651c526adebe24f382ae5ab14b82d0d37c10
bloodweeper <153574225+bloodweeper@users.noreply.github.com>
2023-12-12 08:33:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919127545770, 'Tony', true],
+  ['6281255369012', 'Tony', true], 
   [''] 
 ] //Number of owners
 
